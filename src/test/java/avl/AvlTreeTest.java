e1db9ef6fb4563848cb92089eeaeea0165313d20
josefco-02 <josefco_02@uma.es>
2023-05-12 12:28:18 +0200

    Fixed

--- src/test/java/avl/AvlTreeTest.java
+++ src/test/java/avl/AvlTreeTest.java
@@ -1,3 +1,4 @@
+package avl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,121 +12,188 @@ import org.junit.jupiter.api.*;
  */
 public class AvlTreeTest {
 
-  AvlTree<Integer> avlTree;
-  Comparator<?> comparator;
+    AvlTree<Integer> avlTree;
+    Comparator<?> comparator;
 
-  @BeforeEach
-  public void setUp() throws Exception {
-    comparator = Comparator.comparingInt((Integer o) -> o);
-    avlTree = new AvlTree(comparator);
-  }
-
-  @AfterEach
-  public void tearDown() throws Exception {
-    avlTree = null;
-    comparator = null;
-  }
-
-
-  @Nested
-  @DisplayName("Tests for insertion method, also proofs balance")
-  class insertionTests {
-
-
-    @Test
-    @DisplayName("Should insert into the top of the tree when you call insert method for the first time")
-    public void testInsercionArbolVacio() {
-
-      avlTree.insert(5);
-      assertTrue(avlTree.search(5) != null);
-      int obtainedValue = avlTree.getTop().getItem();
-      int expectedValue = 5;
-
-      assertEquals(expectedValue, obtainedValue);
+    @BeforeEach
+    public void setUp() throws Exception {
+        comparator = Comparator.comparingInt((Integer o) -> o);
+        avlTree = new AvlTree(comparator);
     }
 
-    @Test
-    @DisplayName("Should insert into right of the top node when you add a second node to the tree that is bigger than the top one")
-    public void testInsercionMayorArbolConUnNodo() {
-
-      avlTree.insert(5);
-
-      avlTree.insert(9);
-      assertTrue(avlTree.search(9) != null);
-      int obtainedValue = avlTree.getTop().getRight().getItem();
-      int expectedValue = 9;
-
-      int obtainedValue2 = avlTree.getTop().getItem();
-      int expectedValue2 = 5;
-
-      assertEquals(expectedValue, obtainedValue);
-      assertEquals(expectedValue2, obtainedValue2);
-    }
-
-    @Test
-    @DisplayName("Should insert into left of the top node when you add a second node to the tree that is lower than the top one")
-    public void testInsercionMenorArbolConUnNodo() {
-
-      avlTree.insert(5);
-
-      avlTree.insert(3);
-      assertTrue(avlTree.search(3) != null);
-      int obtainedValue = avlTree.getTop().getLeft().getItem();
-      int expectedValue = 3;
-
-      int obtainedValue2 = avlTree.getTop().getItem();
-      int expectedValue2 = 5;
-
-      assertEquals(expectedValue, obtainedValue);
-      assertEquals(expectedValue2, obtainedValue2);
-    }
-
-    @Test
-    @DisplayName("Shoud insert in the correct position different values when insert method is called several times with different values")
-    public void testInsercionArbolConVariosNodos() {
-
-      avlTree.insert(5);
-      avlTree.insert(2);
-      avlTree.insert(8);
-
-      avlTree.insert(1);
-      assertTrue(avlTree.search(1) != null);
-      int obtainedValue = avlTree.getTop().getLeft().getLeft().getItem();
-      int expectedValue = 1;
-
-      avlTree.insert(7);
-      assertTrue(avlTree.search(7) != null);
-      int obtainedValue2 = avlTree.getTop().getRight().getLeft().getItem();
-      int expectedValue2 = 7;
-
-      assertEquals(expectedValue, obtainedValue);
-      assertEquals(expectedValue2, obtainedValue2);
+    @AfterEach
+    public void tearDown() throws Exception {
+        avlTree = null;
+        comparator = null;
     }
 
 
-  }
+    @Nested
+    @DisplayName("Tests for insertion method, also proofs balance")
+    class insertionTests {
 
-  @Nested
-  @DisplayName("Test order of the tree")
-  class OrderTests {
 
-   @Test
-   @DisplayName("Should return a string with all the items of the tree starting with the top one when the toString method is called")
-    public void inOrderTest () {
+        @Test
+        @DisplayName("Should insert into the top of the tree when you call insert method for the first time")
+        public void testInsercionArbolVacio() {
 
-     avlTree.insert(3);
-     avlTree.insert(7);
-     avlTree.insert(1);
-     avlTree.insert(5);
+            avlTree.insert(5);
+            assertTrue(avlTree.search(5) != null);
+            int obtainedValue = avlTree.getTop().getItem();
+            int expectedValue = 5;
 
-     String expectedValue = " | 3 | 1 | 7 | 5";
-     String obtainedValue = avlTree.toString();
-     assertEquals(avlTree.getTop().getItem(), 3 );
-     assertEquals(expectedValue,obtainedValue);
+            assertEquals(expectedValue, obtainedValue);
+        }
+
+        @Test
+        @DisplayName("Should insert into right of the top node when you add a second node to the tree that is bigger than the top one")
+        public void testInsercionMayorArbolConUnNodo() {
+
+            avlTree.insert(5);
+
+            avlTree.insert(9);
+            assertTrue(avlTree.search(9) != null);
+            int obtainedValue = avlTree.getTop().getRight().getItem();
+            int expectedValue = 9;
+
+            int obtainedValue2 = avlTree.getTop().getItem();
+            int expectedValue2 = 5;
+
+            assertEquals(expectedValue, obtainedValue);
+            assertEquals(expectedValue2, obtainedValue2);
+        }
+
+        @Test
+        @DisplayName("Should insert into left of the top node when you add a second node to the tree that is lower than the top one")
+        public void testInsercionMenorArbolConUnNodo() {
+
+            avlTree.insert(5);
+
+            avlTree.insert(3);
+            assertTrue(avlTree.search(3) != null);
+            int obtainedValue = avlTree.getTop().getLeft().getItem();
+            int expectedValue = 3;
+
+            int obtainedValue2 = avlTree.getTop().getItem();
+            int expectedValue2 = 5;
+
+            assertEquals(expectedValue, obtainedValue);
+            assertEquals(expectedValue2, obtainedValue2);
+        }
+
+        @Test
+        @DisplayName("Shoud insert in the correct position different values when insert method is called several times with different values")
+        public void testInsercionArbolConVariosNodos() {
+
+            avlTree.insert(5);
+            avlTree.insert(2);
+            avlTree.insert(8);
+
+            avlTree.insert(1);
+            assertTrue(avlTree.search(1) != null);
+            int obtainedValue = avlTree.getTop().getLeft().getLeft().getItem();
+            int expectedValue = 1;
+
+            avlTree.insert(7);
+            assertTrue(avlTree.search(7) != null);
+            int obtainedValue2 = avlTree.getTop().getRight().getLeft().getItem();
+            int expectedValue2 = 7;
+
+            assertEquals(expectedValue, obtainedValue);
+            assertEquals(expectedValue2, obtainedValue2);
+        }
+
 
     }
 
-  }
+    @Nested
+    @DisplayName("Test order of the tree")
+    class OrderTests {
+
+        @Test
+        @DisplayName("Should return a string with all the items of the tree starting with the top one when the toString method is called")
+        public void inOrderTest () {
+
+            avlTree.insert(3);
+            avlTree.insert(7);
+            avlTree.insert(1);
+            avlTree.insert(5);
+
+            String expectedValue = " | 3 | 1 | 7 | 5";
+            String obtainedValue = avlTree.toString();
+            assertEquals(avlTree.getTop().getItem(), 3 );
+            assertEquals(expectedValue,obtainedValue);
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Search tests")
+    class SearchTests{
+        @Test
+        @DisplayName("When inserting an item, search method should return the node that contains the item")
+        void WhenInsertingAnItemSearchMethodShouldReturnTheNodeThatContainsTheItemTest() {
+            AvlNode<Integer> node = new AvlNode<>(5);
+            avlTree.insertAvlNode(node);
+
+            Integer expectedValue = 5;
+            Integer obtainedValue = avlTree.search(5).getItem();
+
+            assertEquals(expectedValue, obtainedValue);
+        }
+
+        @Test
+        @DisplayName("When inserting a node, searchNode method should return the node")
+        void WhenInsertingANodeSearchNodeMethodShouldReturnTheNodeTest() {
+            AvlNode<Integer> node = new AvlNode<>(5);
+            avlTree.insertAvlNode(node);
+
+            AvlNode<Integer> expectedValue = node;
+            AvlNode<Integer> obtainedValue = avlTree.searchNode(node);
+
+            assertEquals(expectedValue, obtainedValue);
+        }
+
+        @Test
+        @DisplayName("When searching for a node that is not in the tree, searchNode method should return null")
+        void WhenSearchingForANodeThatIsNotInTheTreeSearchNodeMethodShouldReturnNullTest() {
+
+            AvlNode<Integer> expectedValue = null;
+            AvlNode<Integer> obtainedValue = avlTree.search(3);
+
+            assertEquals(expectedValue, obtainedValue);
+        }
+
+        @Test
+        @DisplayName("When searching closest node with the only item in the tree, searchClosestNode method should return -1")
+        void WhenSearchingClosestNodeWithTheOnlyItemInTheTreeSearchClosestNodeMethodShouldReturnMinusOneTest() {
+            AvlNode<Integer> node = new AvlNode<>(5);
+            avlTree.insertAvlNode(node);
+
+            AvlNode<Integer> insertedNode = new AvlNode<>(3);
+
+            Integer expectedValue = -1;
+            Integer obtainedValue = avlTree.searchClosestNode(insertedNode);
+
+            assertEquals(expectedValue, obtainedValue);
+        }
+
+        @Test
+        @DisplayName("When avlTree contains an item and a successor, findSuccessor method should return the successor")
+        void WhenAvlTreeContainsAnItemAndASuccessorFindSuccessorMethodShouldReturnTheSuccessorTest() {
+            AvlNode<Integer> node1 = new AvlNode<>(5);
+            avlTree.insertAvlNode(node1);
+            AvlNode<Integer> node2 = new AvlNode<>(6);
+            avlTree.insertAvlNode(node2);
+
+            AvlNode<Integer> expectedValue = node2;
+            AvlNode<Integer> obtainedValue = avlTree.findSuccessor(node1);
+
+            assertEquals(expectedValue, obtainedValue);
+        }
+
+    }
 
 
 
