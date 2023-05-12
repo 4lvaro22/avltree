17e23f3691f8867d5baff7b995cce781a23b6eb9
Gil <gilrodriguezjm@uma.es>
2023-05-12 11:40:35 +0200

    Casos de prueba

--- src/test/java/avl/AvlNodeTest.java
+++ src/test/java/avl/AvlNodeTest.java
@@ -8,11 +8,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Created with IntelliJ IDEA. User: Antonio J. Nebro Date: 09/07/13 Time: 15:29
- */
+/* CASOS DE PRUEBA -
+ *   1. Nodo vacío
+ *      1.1. testConstructorAndGetters() -> Comprobar que cuando se crea un nodo, los hijos, el padre y el nodo más cercano son nulos y la altura es 0
+ *   2. Un nodo
+ *      2.1.
+ *   3. Sólo hijo izquierda
+ *      3.1.
+ *   4. Sólo hijo derecha
+ *      4.1
+ *   5. Caso normal (padre e hijos en ambos lados)
+ *      5.1
+ *   2. testSetterAndGetters() -> Comprobar que cuando creas un posible árbol completo (padre, hijos y nodo cercano) con los setters y obtienes el valor con getters, es correcto
+ * */
 public class AvlNodeTest {
 
 
-  
+
 }
