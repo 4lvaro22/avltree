2293015114c1e23fce8647b7e03d418bb549d340
Gil <gilrodriguezjm@uma.es>
2023-05-12 11:09:05 +0200

    Tests

--- src/test/java/avl/AvlNodeTest.java
+++ src/test/java/avl/AvlNodeTest.java
@@ -13,38 +13,6 @@ import org.junit.Test;
  */
 public class AvlNodeTest {
 
-  private AvlNode<Integer> node;
 
-  @Before
-  public void setUp() throws Exception {
-    node = new AvlNode<Integer>(5);
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    node = null;
-  }
-
-  @Test
-  public void testHasLeft() {
-    assertFalse("testHasLeft", node.hasLeft());
-    AvlNode<Integer> node2 = new AvlNode<Integer>(6);
-    node.setLeft(node2);
-    assertTrue("testHasLeft", node.hasLeft());
-  }
-
-  @Test
-  public void testHasRight() {
-    assertFalse("testHasRight", node.hasRight());
-    AvlNode<Integer> node2 = new AvlNode<Integer>(6);
-    node.setRight(node2);
-    assertTrue("testHasRight", node.hasRight());
-  }
-
-  @Test
-  public void shouldSetHeight() {
-    int expectedHeight = 1000213;
-    node.setHeight(expectedHeight);
-    assertEquals("Height is different from expected.", expectedHeight, node.getHeight());
-  }
+  
 }
